3c7de36c6f33a861cdbd668e64e0070c0d3611a2
Mathis Zerbib <mathis.zerbib@gmail.com>
2021-10-15 17:21:30 +0200

    revised files added icons

--- airdropInfinite.sh
+++
@@ -1,2 +0,0 @@
-# PATH="/Users/abderrahimcherkaoui/.local/share/solana/install/active_release/bin:$PATH"
-while sleep 6; do echo $(solana airdrop 10 CdsEDx9BR91w33Z16LMcUPy99XGUfm46iDoZoYPnxCjE --url https://api.devnet.solana.com); done
